88b43fb5b3bfd545622433b1a345ffa0b74e9ccd
Priyansh <priyansh@levels-MacBook-Pro.local>
2023-05-25 16:03:24 +0530

    First commit

---
+++ backend/data/ProductList.js
@@ -0,0 +1,42 @@
+ const ProductList=[{
+        id:"1",
+        title: "Brown eggs",
+        image:"/pictures/brownEggs.jpg",
+        type: "dairy",
+        description: "Raw organic brown eggs in a basket.",
+        price: 28.1,
+        rating: 1
+      }, {
+        id:"2",
+        title: "Sweet fresh stawberry",
+        image:"/pictures/strawberry.jpg",
+        type: "fruit",
+        description: "Sweet fresh stawberry on the wooden table.",
+        price: 29.45,
+        rating: 4
+      }, {
+        id:"3",
+        title: "Asparagus",
+        image:"/pictures/aspagaurus.jpg",
+        type: "vegetable",
+        description: "Asparagus with ham on the wooden table.",
+        price: 18.95,
+        rating: 3
+      }, {
+        id:"4",
+        title: "Green smoothie",
+        image:"/pictures/greensmoothie.jpg",
+        type: "dairy",
+        description: "Glass of green smoothie with quail egg's yolk, served with cocktail tube, green apple and baby spinach leaves over tin surface.",
+        price: 17.68,
+        rating: 4
+      }, {
+        id:"5",
+        title: "Raw legums",
+        image:"/pictures/rawLegumes.jpg",
+        type: "vegetable",
+        description: "Raw legums on the wooden table.",
+        price: 17.11,
+        rating: 2
+      }]
+module.exports=ProductList;
